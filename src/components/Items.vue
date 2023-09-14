df82d616df5f1ccd966478d3e58924ca6199a144
Черепахин Василий <vasi.che@gmail.com>
2023-09-14 13:59:41 +0500

    reverse ниже влияет на отображение выше в div v-for

--- src/components/Items.vue
+++ src/components/Items.vue
@@ -7,28 +7,32 @@ const products = [
 ]
 
 const productsWithRef = ref([
-  {id: 0, name: 'shirt'},
-  {id: 1, name: 'jacket'},
+  {id: 0, name: 'shirt0'},
+  {id: 1, name: 'jacket1'},
   {id: 3, name: 'shoes3'}
 ])
 
 </script>
+
 <template>
-  Example cicle (Without 'ref' <i>products = [{id: 0, name: 'shirt'},)]</i>:
-  <div 
+  <div><u>Example cicle</u> (Without 'ref' <i>products = [{id: 0, name: 'shirt'},)]</i>:</div>
+  <div class="greetings"
     v-for='(product, index) in products' 
     :key='product.id' 
   >
     #index:{{ index + 1}}  id:{{product.id }}  name:{{ product.name }}
   </div>
-  Example cicle (WITH 'ref' <i>productsWithRef = ref([{id: 0, name: 'shirt'},...]))</i>:
+  <u>Example cicle</u> (WITH 'ref' <i>productsWithRef = ref([{id: 0, name: 'shirt'},...]))</i>:
   <div 
     v-for='(product, index) in productsWithRef' 
     :key='product.id' 
   >
     #index:{{ index + 1}}  id:{{product.id }}  name:{{ product.name }}
   </div>
-  <div>{{ productsWithRef.reverse() }}</div>
-  <div>{{ productsWithRef.reverse() }}</div>
-  <!-- <div>{{ productsWithRef.reverse() }}</div> -->
+  <!-- reverse ниже влияет на отображение выше в div v-for-->
+  <div><u>productsWithRef.reverse() 1 раз</u>:<br/> {{ productsWithRef.reverse() }}</div>
+  <!--[ { "id": 3, "name": "shoes3" }, { "id": 1, "name": "jacket" }, { "id": 0, "name": "shirt" } ]-->
+  <div><u>productsWithRef.reverse() 2 раз</u>:<br/>{{ productsWithRef.reverse() }}</div>
+  <!--[ { "id": 0, "name": "shirt" }, { "id": 1, "name": "jacket" }, { "id": 3, "name": "shoes3" } ]-->
+  <div><u>products.reverse()</u>:<br/> {{ products.reverse() }}</div>
 </template>
