947ca927c4e3135adf448c0af059d23938a7b279
Черепахин Василий <vasi.che@gmail.com>
2023-09-14 19:11:52 +0500

    Нормально работает с одним списком

--- src/App.vue
+++ src/App.vue
@@ -7,17 +7,15 @@ import ItemsVue from './components/Items.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
+    <!-- <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" /> -->
     <div class="wrapper">
       <HelloWorld msg="You did it!" name="Vasili"/>
     </div>
   </header>
-
+  <br/>
   <main>
-    <FeaturesVue />
+    <!-- <FeaturesVue /> -->
     <ItemsVue />
-    <!-- <ItemsVue /> -->
   </main>
 </template>
 
--- src/components/Items.vue
+++ src/components/Items.vue
@@ -7,32 +7,16 @@ const products = [
 ]
 
 const productsWithRef = ref([
-  {id: 0, name: 'shirt0'},
-  {id: 1, name: 'jacket1'},
-  {id: 3, name: 'shoes3'}
+  {id: 10, name: 'shirt10'},
+  {id: 20, name: 'jacket20'},
+  {id: 30, name: 'shoes30'},
 ])
-
 </script>
 
 <template>
-  <div><u>Example cicle</u> (Without 'ref' <i>products = [{id: 0, name: 'shirt'},)]</i>:</div>
-  <div class="greetings"
-    v-for='(product, index) in products' 
-    :key='product.id' 
-  >
-    #index:{{ index + 1}}  id:{{product.id }}  name:{{ product.name }}
+  <div><h2>Tests</h2></div>
+  <div><h3><u>productsWithRef:</u></h3>
+    <!--вывод на экран 0-->
+    {{ productsWithRef }}
   </div>
-  <u>Example cicle</u> (WITH 'ref' <i>productsWithRef = ref([{id: 0, name: 'shirt'},...]))</i>:
-  <div 
-    v-for='(product, index) in productsWithRef' 
-    :key='product.id' 
-  >
-    #index:{{ index + 1}}  id:{{product.id }}  name:{{ product.name }}
-  </div>
-  <!-- reverse ниже влияет на отображение выше в div v-for-->
-  <div><u>productsWithRef.reverse() 1 раз</u>:<br/> {{ productsWithRef.reverse() }}</div>
-  <!--[ { "id": 3, "name": "shoes3" }, { "id": 1, "name": "jacket" }, { "id": 0, "name": "shirt" } ]-->
-  <div><u>productsWithRef.reverse() 2 раз</u>:<br/>{{ productsWithRef.reverse() }}</div>
-  <!--[ { "id": 0, "name": "shirt" }, { "id": 1, "name": "jacket" }, { "id": 3, "name": "shoes3" } ]-->
-  <div><u>products.reverse()</u>:<br/> {{ products.reverse() }}</div>
 </template>
